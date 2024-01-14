bf9960d27f2ee6c06a498f3708a9ae3883a7bdb2
rohitPandey469 <rd8614196@gmail.com>
2024-01-14 11:48:52 +0530

    Added the summaryEditor

--- src/pages/CreateTask/components/SummaryEdit.jsx
+++ src/pages/CreateTask/components/SummaryEdit.jsx
@@ -1,23 +1,47 @@
+import React, {  useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import styles from "./SummaryEdit.module.css";
 
-// for further extension
+export default function SummaryEdit({ description, handleChange }) {
+  const [value, setValue] = useState(description);
 
-import styles from './SummaryEdit.module.css'
+  function handleBlur() {
+    console.log("Loses FOCUS!!!");
+    // Update the description here
+    if (handleChange !== undefined) {
+      handleChange({
+        target: {
+          name: "description",
+          value,
+        },
+      });
+    }
+  }
 
-export default function SummaryEdit({description, handleChange}){
-    return(
-        <>
-            {/* description textarea */}
-            <textarea 
-                id="description" 
-                type="text" 
-                value={description} 
-                className={styles.summaryBox}
-                name="description" 
-                onChange={handleChange} 
-                placeholder="Task Summary"
-                rows="7"
-                required
-            />
-        </>
-    )
+  const modules = {
+    toolbar: [
+      [{ font: [] }],
+      [{ align: [] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ color: [] }, { background: [] }],
+      [{ script: "sub" }, { script: "super" }],
+      ["blockquote", "code-block"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+  return (
+    <ReactQuill
+      className={styles.summaryBox}
+      value={value}
+      modules={modules}
+      theme="snow"
+      onChange={setValue}
+      placeholder="Task Summary"
+      onBlur={handleBlur}
+    />
+  );
 }
